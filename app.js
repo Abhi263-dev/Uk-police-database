784bc7092c28a65568103048b0840c97b58ed1e1
Abhishek Dhangar <abhishek.dhangar@cubastion.com>
2023-11-23 17:02:42 +0530

    Changed Js to jquery

--- app.js
+++ app.js
@@ -1,74 +1,128 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    getData();
-    // console.log("calling");
-}) 
+// document.addEventListener('DOMContentLoaded', function () {
+//     getData();
+//     // console.log("calling");
+// }) 
 
-let data;
+// let data;
 
-async function getData() {
-    const response = await fetch(
-      `https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2023-01`
-    );
-    data = await response.json();
-    // console.log(data);
-    }
+// async function getData() {
+//     const response = await fetch(
+//       `https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2023-01`
+//     );
+//     data = await response.json();
+//     // console.log(data);
+//     }
 
 
-function filterData (){
-    let s=document.getElementById("input").value;
-    // console.log(s);
+// function filterData (){
+//     let s=document.getElementById("input").value;
+//     // console.log(s);
 
-    function filterAndLimit(data, category, limit) {
-        const filteredData = data.filter(item => item.category === category);
-        const limitedData = filteredData.slice(0, limit);
-        return limitedData;
-      }
+//     function filterAndLimit(data, category, limit) {
+//         const filteredData = data.filter(item => item.category === category);
+//         const limitedData = filteredData.slice(0, limit);
+//         return limitedData;
+//       }
 
-    const filteredAndLimitedData = filterAndLimit(data, s, 1000);
-    console.log(filteredAndLimitedData);
+//     const filteredAndLimitedData = filterAndLimit(data, s, 1000);
+//     console.log(filteredAndLimitedData);
 
-    // var filteredHtml = filteredAndLimitedData.map(function(person) {
-    //     // console.log(person)
-    //     return (`<div id="ans"> <div id="ans2"> ${person.location.street.name},</div><div id="ans3">${person.outcome_status.date},</div><div id="ans4">${person.outcome_status.category}</div></div>`);
+//     // var filteredHtml = filteredAndLimitedData.map(function(person) {
+//     //     // console.log(person)
+//     //     return (`<div id="ans"> <div id="ans2"> ${person.location.street.name},</div><div id="ans3">${person.outcome_status.date},</div><div id="ans4">${person.outcome_status.category}</div></div>`);
       
-    //     //experiment code
-    //   });
-      function generateTableHTML(data) {
-        var tableHTML = '<table border="5" color="purple"><thead><tr><th>Street Name</th><th>Date</th><th>Status</th></tr></thead><tbody>';
+//     //     //experiment code
+//     //   });
+//       function generateTableHTML(data) {
+//         var tableHTML = '<table border="5" color="purple"><thead><tr><th>Street Name</th><th>Date</th><th>Status</th></tr></thead><tbody>';
   
-      filteredAndLimitedData.forEach(function (item) {
-          tableHTML += '<tr>';
-          tableHTML += '<td id="ans2">' + item.location.street.name + '</td>';
-          tableHTML += '<td id="ans3">' + item.outcome_status.date + '</td>';
-          tableHTML += '<td id=ans4>' + item.outcome_status.category + '</td>';
-          tableHTML += '</tr>';
-        });
+//       filteredAndLimitedData.forEach(function (item) {
+//           tableHTML += '<tr>';
+//           tableHTML += '<td id="ans2">' + item.location.street.name + '</td>';
+//           tableHTML += '<td id="ans3">' + item.outcome_status.date + '</td>';
+//           tableHTML += '<td id=ans4>' + item.outcome_status.category + '</td>';
+//           tableHTML += '</tr>';
+//         });
   
-        tableHTML += '</tbody></table>';
-        //console.log(tableHTML);
-        return tableHTML;
-      }
+//         tableHTML += '</tbody></table>';
+//         //console.log(tableHTML);
+//         return tableHTML;
+//       }
   
-      // // Get the container element
-      var tableContainer = document.getElementById('table-container');
+//       // // Get the container element
+//       var tableContainer = document.getElementById('table-container');
       
-      // // Set the innerHTML of the container with the generated table HTML
-      tableContainer.innerHTML = generateTableHTML(data);
+//       // // Set the innerHTML of the container with the generated table HTML
+//       tableContainer.innerHTML = generateTableHTML(data);
        
-      // var resultContainer = document.getElementById('result');
-      // resultContainer.innerHTML = filteredHtml.join('');
-}   
+//       // var resultContainer = document.getElementById('result');
+//       // resultContainer.innerHTML = filteredHtml.join('');
+// }   
 
-// function filterDataByCategory(data1, cat) {
-//     return data1.filter((item )=>
-//         {
-//             console.log(item);
-//             return item.category === cat;
-//         } );
-//   }
+// // function filterDataByCategory(data1, cat) {
+// //     return data1.filter((item )=>
+// //         {
+// //             console.log(item);
+// //             return item.category === cat;
+// //         } );
+// //   }
 
-//   const filteredData = filterDataByCategory(data, 'burglary');
-//   console.log(filteredData) ;
+// //   const filteredData = filterDataByCategory(data, 'burglary');
+// //   console.log(filteredData) ;
 
+$(document).ready(function () {
 
+  $("#search").click(function(){
+    getData();
+
+    let data;
+  
+    async function getData() {
+      try {
+          const response = await $.ajax({
+              url: 'https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2023-01',
+              method: 'GET',
+              dataType: 'json'
+          });
+  
+          data = response;
+          filterData();
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
+  }
+    function filterData() {
+        let s = $("#input").val();
+  
+        function filterAndLimit(data, category, limit) {
+            const filteredData = data.filter(item => item.category === category);
+            const limitedData = filteredData.slice(0, limit);
+            return limitedData;
+        }
+  
+        const filteredAndLimitedData = filterAndLimit(data, s, 1000);
+        console.log(filteredAndLimitedData);
+  
+        function generateTableHTML(data) {
+            var tableHTML = '<table border="5" color="purple"><thead><tr><th>Street Name</th><th>Date</th><th>Status</th></tr></thead><tbody>';
+  
+            filteredAndLimitedData.forEach(function (item) {
+                tableHTML += '<tr>';
+                tableHTML += '<td id="ans2">' + item.location.street.name + '</td>';
+                tableHTML += '<td id="ans3">' + item.outcome_status.date + '</td>';
+                tableHTML += '<td id=ans4>' + item.outcome_status.category + '</td>';
+                tableHTML += '</tr>';
+            });
+  
+            tableHTML += '</tbody></table>';
+            return tableHTML;
+        }
+  
+        var tableContainer = $('#table-container');
+  
+        tableContainer.html(generateTableHTML(data));
+    }
+  });
+ 
+});
